df2c0d4c994c0cf519c21f98ce2adb9dcfa25c40
isaacgoodfellow <goodfellowisaac@gmail.com>
2017-07-06 15:24:49 -0700

    Linux Changes + logging

--- projects/web/cef/src/private/web_handler.cc
+++ projects/web/cef/src/private/web_handler.cc
@@ -518,8 +518,12 @@ void WebHandler::sendKeyEvent(const int browserId, const int state, int windows_
 
 		default:
 			isChar = true;
-			keyEvent.windows_key_code = character;
-			keyEvent.character = keyEvent.unmodified_character = character;
+#ifdef WIN32
+			keyEvent.native_key_code = windows_key_code;
+#endif
+			keyEvent.unmodified_character = character;
+			keyEvent.character =  character;
+			std::cout << "It's a char!" << std::endl;
 
 #ifndef  WIN32
 			DS_LOG_WARNING("Setting a key-code of " << keyEvent.windows_key_code);
@@ -559,7 +563,7 @@ void WebHandler::sendKeyEvent(const int browserId, const int state, int windows_
 				keyEvent.type = KEYEVENT_KEYDOWN;
 			}
 
-			//std::cout << "Key down: " << isChar << " " << character << " " << keyEvent.windows_key_code << " " << keyEvent.native_key_code << std::endl;
+			std::cout << "Key down: " << isChar << " " << character << " " << keyEvent.windows_key_code << " " << keyEvent.native_key_code << std::endl;
 
 			// This can be called on any thread
 			browserHost->SendKeyEvent(keyEvent);
@@ -569,7 +573,7 @@ void WebHandler::sendKeyEvent(const int browserId, const int state, int windows_
 		} else {
 			keyEvent.type = KEYEVENT_KEYUP;
 
-			//std::cout << "Key up: " << isChar << " " << character << " " << keyEvent.windows_key_code << " " << keyEvent.native_key_code << std::endl;
+			std::cout << "Key up: " << isChar << " " << character << " " << keyEvent.windows_key_code << " " << keyEvent.native_key_code << std::endl;
 			// This can be called on any thread
 			browserHost->SendKeyEvent(keyEvent);
 #ifndef  WIN32
